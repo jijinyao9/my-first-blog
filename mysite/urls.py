8844a1c6299871404aa91ee7c003b0f13038166c
Your Name <you@example.com>
2020-04-24 21:18:26 +0800

    django2.0版本更新

--- mysite/urls.py
+++ mysite/urls.py
@@ -5,6 +5,9 @@ urlpatterns = [
     # Examples:
     # url(r'^$', 'mysite.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
+    
+    #这是由于在django2.0后不支持使用在urls.py中写
 
-    url(r'^admin/', include(admin.site.urls)),
+    #url(r'^admin/', include(admin.site.urls)),
+    path('admin/', admin.site.urls),
 ]
