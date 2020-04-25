dc07889a7fbead2898ed282d21d3480263ca7d48
Your Name <you@example.com>
2020-04-25 10:45:32 +0800

    path

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,11 +1,12 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.urls import path
 
 urlpatterns = [
     # Examples:
     # url(r'^$', 'mysite.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-    
+
     #这是由于在django2.0后不支持使用在urls.py中写
 
     #url(r'^admin/', include(admin.site.urls)),
