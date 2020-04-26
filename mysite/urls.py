f6812e281faacfbd229d6784e9ab503da2275f24
Your Name <you@example.com>
2020-04-26 20:37:12 +0800

    添加基本功能，一个简单的博客

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import include, url
 from django.contrib import admin
-from django.urls import path
+#from django.urls import path
 
 urlpatterns = [
     # Examples:
@@ -8,7 +8,12 @@ urlpatterns = [
     # url(r'^blog/', include('blog.urls')),
 
     #这是由于在django2.0后不支持使用在urls.py中写
-
     #url(r'^admin/', include(admin.site.urls)),
-    path('admin/', admin.site.urls),
+
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'', include('blog.urls')),
+
+    #path('admin/', admin.site.urls),
+
+
 ]
